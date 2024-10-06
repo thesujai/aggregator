dfe96e568274cd9546f46de6cae68ab37a97adfb
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-10-06 12:38:27 +0530

    start the loop from len-1

--- middleware.go
+++ middleware.go
@@ -8,8 +8,8 @@ import (
 type Middleware func(http.Handler) http.Handler
 
 func applyMiddleware(handler http.Handler, middlewares ...Middleware) http.Handler {
-	for i := len(middlewares); i >= 0; i-- {
-		handler = middlewares[0](handler)
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		handler = middlewares[i](handler)
 	}
 	return handler
 }
@@ -17,5 +17,6 @@ func applyMiddleware(handler http.Handler, middlewares ...Middleware) http.Handl
 func logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Printf("%v %v \n", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
 	})
 }

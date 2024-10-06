366c322a20b903d48a94e8851b91cc13127e14c5
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-10-06 14:51:17 +0530

    create a apiConfig struct to connect with the database

--- main.go
+++ main.go
@@ -1,14 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/thesujai/aggregator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
+type apiConfig struct {
+	DB *database.Queries
+}
+
+func GetAPIConfig() *apiConfig {
+	dbURL, ok := os.LookupEnv("DB_URL")
+	if !ok {
+		log.Fatal("DB_URL environment variable is not set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbQueries := database.New(db)
+
+	apiCgf := apiConfig{
+		DB: dbQueries,
+	}
+
+	return &apiCgf
+}
+
 func main() {
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		log.Fatal("PORT environment variable is not set")
+	}
+
 	serv := &http.Server{
-		Addr:    ":8012",
+		Addr:    ":" + port,
 		Handler: registerRoutes(),
 	}
 	fmt.Printf("Server Running and Listening on %v\n", serv.Addr)

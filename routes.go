caea8830d165d6ea25324671b83781ba16cf1cdf
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-10-09 14:13:47 +0530

    versioning to api and seperate public and protected mux

--- routes.go
+++ routes.go
@@ -3,11 +3,22 @@ package main
 import "net/http"
 
 func registerRoutes() http.Handler {
-	mux := http.NewServeMux()
 	cfg := GetAPIConfig()
-	mux.Handle("GET /healthz", http.HandlerFunc(systemHealth))
-	mux.Handle("POST /users", http.HandlerFunc(cfg.createUser))
-	mux.Handle("GET /users", http.HandlerFunc(cfg.getUser))
-	mux_with_middleware := use(mux, logger)
-	return mux_with_middleware
+
+	publicMux := http.NewServeMux()
+	publicMux.Handle("GET /healthz", http.HandlerFunc(systemHealth))
+	publicMux.Handle("POST /users", http.HandlerFunc(cfg.createUser))
+	publicMux.Handle("GET /users", http.HandlerFunc(cfg.getUser))
+
+	protectedMux := http.NewServeMux()
+	protectedMux.Handle("POST /feeds", http.HandlerFunc(cfg.createFeed))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", use(protectedMux, cfg.authUser))
+	mux.Handle("/public/", http.StripPrefix("/public", publicMux))
+
+	v1_mux := http.NewServeMux()
+	v1_mux.Handle("/v1/", http.StripPrefix("/v1", use(mux, cfg.logger)))
+
+	return v1_mux
 }

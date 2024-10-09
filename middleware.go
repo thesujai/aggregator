83db51771501862d50ceb6d351535fd4a017bbc4
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-10-09 14:13:22 +0530

    fea: authentication middleware

--- middleware.go
+++ middleware.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/thesujai/aggregator/internal/auth"
 )
 
 type Middleware func(http.Handler) http.Handler
@@ -15,10 +17,29 @@ func use(handler http.Handler, middlewares ...Middleware) http.Handler {
 	return handler
 }
 
-func logger(next http.Handler) http.Handler {
+func (cfg *apiConfig) logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		log.Printf("%s %s %v", r.Method, r.URL.Path, time.Since(start))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) authUser(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		api_key, err := auth.GetApiKey(r)
+
+		if err != nil {
+			http.Error(w, err.Error(), 400)
+			return
+		}
+
+		user_id, err := cfg.DB.GetUserId(r.Context(), api_key)
+		if err != nil {
+			http.Error(w, "user doesn't exists", 400)
+			return
+		}
+		w.Header().Set("userID", user_id.String())
 		next.ServeHTTP(w, r)
 	})
 }

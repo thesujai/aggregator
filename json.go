063f524a025afa6e7b7239f8db369893feb1b96c
Sujai Kumar Gupta <b210031@nitsikkim.ac.in>
2024-10-10 23:36:05 +0530

    add a respond with json error method

--- json.go
+++ json.go
@@ -6,6 +6,29 @@ import (
 	"net/http"
 )
 
+type Error struct {
+	Error string `json:"error"`
+}
+
+func RespondWithError(w http.ResponseWriter, code int, err error) {
+	w.Header().Set("Content-Type", "application/json")
+	if code < 500 {
+		fmt.Println("Unexpected Error: ", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	json_body, err := json.Marshal(Error{
+		Error: string(err.Error()),
+	})
+	if err != nil {
+		fmt.Println("Unexpected Error: ", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(json_body)
+}
+
 func RespondWithJSON(w http.ResponseWriter, code int, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	json_body, err := json.Marshal(data)
